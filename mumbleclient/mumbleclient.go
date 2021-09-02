200603931055fb642cc2e7649e8f0aa0b96aed1b
360ied <19516527+360ied@users.noreply.github.com>
2021-09-01 23:23:51 -0400

    add mumble version encoder

--- mumbleclient/mumbleclient.go
+++ mumbleclient/mumbleclient.go
@@ -1,1 +1,14 @@
 package mumbleclient
+
+import (
+	"bufio"
+	"net"
+)
+
+type MumbleClient struct {
+	conn       net.Conn      // TLS conn
+	connReader *bufio.Reader // TLS conn reader
+
+	Username string
+	Password string
+}
