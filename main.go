295863364a2716791eba5ad2611dcd13c6164d3e
360ied <19516527+360ied@users.noreply.github.com>
2021-09-02 13:28:38 -0400

    add main

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"mumbletest/mumblebot"
+	"os"
+)
 
+func main() {
+	mumbleServerAddress := os.Getenv("MUMBLETEST_SERVER_ADDR")
+	mumbleUsername := os.Getenv("MUMBLETEST_USERNAME")
+	mumblePassword := os.Getenv("MUMBLETEST_PASSWORD")
+
+	mumbleBot := mumblebot.MumbleBot{
+		ServerIP: mumbleServerAddress,
+		Username: mumbleUsername,
+		Password: mumblePassword,
+	}
+
+	if err := mumbleBot.Start(); err != nil {
+		panic(err)
+	}
 }
